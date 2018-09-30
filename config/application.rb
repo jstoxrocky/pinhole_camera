95e5154b704320144478aaaffa4a7aadde9681a6
jstoxrocky <joestox@gmail.com>
2018-09-30 19:34:50 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,8 @@ module PinholeCamera
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:8080'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
       end
     end
-
-
   end
 end
